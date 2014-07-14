2ea988d3bd094d48c118ab869f7924e0684e37e9
Dylan Linthorne <DylanLinthorne@live.ca>
2014-07-14 10:39:01 +0200

    commit

--- graph.cxx
+++ graph.cxx
@@ -8,17 +8,17 @@ void graph()
 	gROOT -> SetStyle("Plain");
 	TCanvas *c1 = new TCanvas();
 	c1 -> SetGrid();
-	TGraphErrors data("data-carb-all.txt", "%lg %lg %lg");
-	//TGraphErrors data("data.txt", "%lg %lg %lg");
+	TGraphErrors data("Pi0P_DMT_E_300.txt","%lg %lg %lg");
+	//TGraphErrors data("data-carb-all2.txt", "%lg %lg %lg");
 	data.SetTitle("Frozen Spin Target Polarization/Asymmetry ;time [days]; #Sigma P_{#gamma}");
 	data.SetMarkerStyle(kCircle);
 	data.SetFillColor(0);
 	data.SetLineColor(56);
 	data.Fit("pol1","","",1,8);
 	data.DrawClone();
-	c1->Print("Fit.png", "png"); 
+	c1->Print("new.png", "png"); 
 
-	TFile f("data.root","recreate"); //Open file, then write histo to it.
+	TFile f("new.root","recreate"); //Open file, then write histo to it.
   	data.Write();
 }
 	
